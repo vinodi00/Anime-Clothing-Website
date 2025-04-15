31903c17d12ebd5958af82448de59be9fa859dc1
vinodi saumya <114313362+vinodi00@users.noreply.github.com>
2025-04-16 00:34:55 +0530

    logo

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,16 +1,36 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { FaUser, FaShoppingCart, FaSearch } from 'react-icons/fa';
+import logo from '../assets/logo.png'; // ensure logo path is correct
+import './Nav.css';
 
 function Navbar() {
   return (
-    <nav>
-      <Link to="/">Home</Link>
-      <Link to="/shop/Anime">Anime</Link>
-      <Link to="/shop/Fastlane">Fastlane</Link>
-      <Link to="/shop/BasicTees">Basic Tees</Link>
-      <Link to="/cart">Cart</Link>
-      <Link to="/login">Login</Link>
-    </nav>
+    <header className="navbar">
+      <div className="navbar-container">
+        {/* Logo */}
+        <div className="navbar-logo">
+          <Link to="/">
+            <img src={logo} alt="Tora Tee Logo" />
+          </Link>
+        </div>
+
+        {/* Navigation Links */}
+        <nav className="navbar-links">
+          <Link to="/">Home</Link>
+          <Link to="/shop/basic-tees">Basic Tees</Link>
+          <Link to="/shop/hoodies">Hoodies</Link>
+          <Link to="/custom-order">Custom Order</Link>
+        </nav>
+
+        {/* Icons */}
+        <div className="navbar-icons">
+          <FaSearch className="icon" title="Search" />
+          <FaUser className="icon" title="Account" />
+          <FaShoppingCart className="icon" title="Cart" />
+        </div>
+      </div>
+    </header>
   );
 }
 
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
-
+import heroImage from '../assets/slide1.png'; // replace with your image name
 
 function Home() {
   return (
-    <div>
+    <>
 
-    </div>
+
+      <div className="home-hero">
+        <img src={heroImage} alt="Hero" className="hero-image" />
+      </div>
+    </>
   );
 }
 
